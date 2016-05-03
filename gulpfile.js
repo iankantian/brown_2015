76c8841d60b9e5daec76a86b80b75fbbaaef8f40
iankantian <iankantian@gmail.com>
2016-05-03 13:26:32 -0700

    added linting via ESLint, but disabled errors for console for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 // gulp dependencies go here
 
 gulp.task('default', function(){
+    // run eslint
+    gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format());
     // node src
     gulp.src('es6/**/*.js')
         .pipe(babel())
