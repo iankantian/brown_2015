ef76ac439e5e36b666de664036465b618253993d
iankantian <iankantian@gmail.com>
2016-06-10 17:15:07 -0700

    added morbid game of self destruction

--- es6/my_test.js
+++ es6/my_test.js
@@ -2,59 +2,55 @@
  * Created by joshuabrown on 5/3/16.
  */
 'use strict';
-
-const EventEmitter = require('events').EventEmitter;
-
-class CountDown extends EventEmitter{
-    constructor( seconds, superstitious ){
-        super();
-        this.seconds = seconds;
-        this.superstitious = !!superstitious;
-    }
-    go(){
-        const countdown = this;
-        const timeoutIds = [];
-        return new Promise(function(resolve, reject){
-            for(let i = countdown.seconds; i >= 0; i-- ){
-                timeoutIds.push( setTimeout(function(){
-                        if( countdown.superstitious && i === 13 ){
-                            timeoutIds.forEach( clearTimeout );
-                            return reject( new Error('not counting that one!'));
-                        }
-                        countdown.emit('tick', i );
-                        if( i === 0 ) resolve();
-                }, ( countdown.seconds - i ) * 1000 ) );
-            }
+(function(){
+    function self_destruct(time) {
+        return new Promise(function (resolve, reject) {
+            let destruction_display = document.getElementById('destruction_display');
+            destruction_display.innerHTML = `Promise of self_destruct given. ${time} seconds until detonation!` + '<br>' + `Press Right Arrow to cancel self_destruct!\nPress Left Arrow if you want to get it over with!`;
+            let timer = setTimeout(function () {
+                document.body.style.backgroundColor = 'red';
+                destruction_display.innerHTML = 'Detonation! self_destruct Promise fulfilled by resolution';
+                resolve('Detonation! Promise of self_destruct resolved at scheduled time!');
+            }, time * 1000);
+            let listener = function (event) {
+                let self = this;
+                if (event.keyCode == 39) {
+                    // Right was pressed, cancel it
+                    clearTimeout(timer);
+                    destruction_display.innerHTML = 'Cancelling self_destruct, Promise fulfilled by rejection';
+                    reject(self);
+                }
+                else if (event.keyCode == 37) {
+                    // Left was pressed, do it now!
+                    clearTimeout(timer);
+                    document.body.style.backgroundColor = 'red';
+                    destruction_display.innerHTML = 'Immediate self_destruct, Promise fulfilled by resolving';
+                    resolve(self);
+                }
+            };
+            document.addEventListener('keydown', listener);
         });
     }
-}
 
-function launch (){
-    return new Promise(function( resolve, reject ){
-        console.log('liftoff!');
-        if( Math.random()>.5 ){
-            // failure condition selected!
-            reject('broke!');
+    function init( time ){
+        self_destruct( time ).then(
+            // resolution!
+            function (listener) {
+                // clean up those events
+                document.removeEventListener('keydown', listener);
+            },
+            // rejection!
+            function (listener) {
+                // clean up those events
+                document.removeEventListener('keydown', listener);
+            }
+        );
+    }
+
+    let readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === 'complete') {
+            clearInterval(readyStateCheckInterval);
+            init( 15 );
         }
-        setTimeout(function(){
-            resolve('in orbit!');
-        }, 2*1000 );
-    });
-}
-
-const mission = new CountDown( 5, false )
-    .on( 'tick', function( i ){
-        if( i > 0 ) console.log( i + '...');
-    });
-
-mission.go()
-    .then( launch )
-    .then( function(msg){
-        console.log(msg);
-    })
-    .then( function(){
-        console.log('go!');
-    })
-    .catch(function(err){
-        console.log('houston, we have a problem', err.message);
-    });
+    }, 10);
+})();
